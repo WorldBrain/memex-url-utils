e454f825224e2d79db3b40e56d427a9446bcf31f
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2019-09-30 15:26:20 +1000

    Ensure all types from modules are exported from main entrypoint

--- ts/index.ts
+++ ts/index.ts
@@ -1,2 +1,5 @@
 export { default as normalizeUrl } from './normalize'
 export { default as extractUrlParts } from './extract-parts'
+
+export * from './normalize/types'
+export * from './extract-parts/types'
